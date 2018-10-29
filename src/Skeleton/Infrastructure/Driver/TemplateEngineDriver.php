c48fc95bad884d8932bbb5ac163ffd9f0617776a
Assasz <pawel.assasz678@gmail.com>
2018-10-29 19:45:23 +0100

    [TemplateEngineDriver] Add methods declaractions

--- src/Skeleton/Infrastructure/Driver/TemplateEngineDriver.php
+++ src/Skeleton/Infrastructure/Driver/TemplateEngineDriver.php
@@ -74,4 +74,38 @@ class TemplateEngineDriver extends AbstractDriver implements DriverInterface, Te
 
         return self::$driverInstance;
     }
+
+    /**
+     * Renders given view
+     *
+     * @param string $view
+     * @param array $params
+     * @return string
+     */
+    public function render(string $view, array $params = []): string
+    {
+        self::$engineInstance->render($view, $params);
+    }
+
+    /**
+     * Displays given view
+     *
+     * @param string $view
+     * @param array $params
+     */
+    public function display(string $view, array $params = []): void
+    {
+        self::$engineInstance->display($view, $params);
+    }
+
+    /**
+     * Adds global to template engine
+     *
+     * @param string $name
+     * @param mixed $value
+     */
+    public function addGlobal(string $name, $value): void
+    {
+        self::$engineInstance->addGlobal($name, $value);
+    }
 }
