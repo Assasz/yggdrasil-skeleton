12bbddefc32d0accd59a27eed7c66479a031480d
Paweł Antosiak <pawel.antosiak@sportdata.pl>
2019-02-21 23:15:49 +0100

    Refactoring

--- src/Skeleton/Application/DriverInterface/RouterInterface.php
+++ src/Skeleton/Application/DriverInterface/RouterInterface.php
@@ -12,8 +12,8 @@ interface RouterInterface
     /**
      * Returns absolute path for requested action
      *
-     * @param string $alias Alias of action like Controller:action
-     * @param array $params Set of action parameters
+     * @param string $alias  Alias of action like Controller:action
+     * @param array  $params Set of action parameters
      * @return string
      */
     public function getQuery(string $alias, array $params = []): string;
