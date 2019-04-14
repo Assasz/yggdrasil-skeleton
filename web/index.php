2f9baa684ac6b0d3320f242c805b1be212ee3c8e
Assasz <pawel.assasz678@gmail.com>
2019-04-14 14:27:41 +0200

    Add annotations autoloading

--- web/index.php
+++ web/index.php
@@ -1,10 +1,13 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
 use Skeleton\Infrastructure\Configuration\AppConfiguration;
 use Yggdrasil\Core\Kernel;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+AnnotationRegistry::registerLoader([$loader, 'loadClass']);
 
 $appConfiguration = new AppConfiguration();
 
